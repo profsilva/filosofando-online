639f0839634d976b65ae9fa4dede0822c0fca417
stackbit-projects <projects@stackbit.com>
2020-06-18 01:51:14 +0000

    Update configuration

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,11 +7,11 @@ set -v
 initialGitHash=$(git rev-list --max-parents=0 HEAD)
 node ./studio-build.js $initialGitHash &
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
-npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5eeac8245ad8d000194779cb/webhook/build/pull > /dev/null
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5eeac8245ad8d000194779cb
+curl -s -X POST https://api.stackbit.com/project/5eeac8245ad8d000194779cb/webhook/build/ssgbuild > /dev/null
 gatsby build
 wait
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5eeac8245ad8d000194779cb/webhook/build/publish > /dev/null
 echo "Stackbit-build.sh finished build"
